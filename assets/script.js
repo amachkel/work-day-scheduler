46cef4b7c850dd9ac17248f7a1f1149b83c9a7ea
amachkel <amachkel@gmail.com>
2022-01-30 13:54:25 -0500

    text input gets saved locally and rendered to page upon refresh. Current hour is compared to dataId and respective class with its css properties are appended. Everything is functional.

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,57 @@
-const today = moment();
-$("#currentDay").text(today.format('LLLL'));
+//displays current day and time
+getCurrentTime = () => {
+  const today = moment();
+  $("#currentDay").text(today.format("LLLL"));
+};
 
-//if time has passed, make it gray; else if time is
-//present, make it red. else, make it green.
-//for loop comparing momentjs time with hour in <span>?
+getInit = () => {
+  //loop through local storage for anything that exists
+  for (let i = 0; i < localStorage.length; i++) {
+    //index of key
+    const key = localStorage.key(i);
+    //gets value from key and saves in variable named description
+    const description = localStorage.getItem(key);
 
-const currentHour = moment();
-console.log(currentHour.format('LT'));
-let hourEl = document.getElementsByClassName("hour");
-determineColor = () => {
-//need to convert string to number to compare hourEl and current hour
-if (currentHour > hourEl)
-}
+    //input's attribute `data-id` is its corresponding key
+    $(`input[data-id=${key}]`).val(description);
+  }
+};
 
-//select element by class
-//$('.className')
-//select element by ID
-//$('#idName')
+attachBtnClicks = () => {
+  const saveBtnEl = $(".saveBtn");
+  saveBtnEl.on("click", function () {
+    const button = $(this); //gets reference to what was just clicked.
+    const descriptionEl = button.closest(".row").find(".description"); //gets closest with class=description
+    const description = descriptionEl.val(); //value of text input
+    const dataId = descriptionEl.attr("data-id"); //get attribute from descriptionEl
+    
+    //dataId is the key, description is the value.
+    localStorage.setItem(`${dataId}`, description); //store description and its data-id (using string interpolation)
+  });
+};
+
+//compare .description's data-id to current hour and
+//append respective class with its css properties to color-code based on time.
+reserveTimeStates = () => {
+  $(".description").each(function () {
+    let dataId = $(this).attr("data-id");
+    let currentHour = moment().hour();
+
+    if (dataId < currentHour) {
+      $(this).addClass("past");
+    } else if (dataId == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+};
+
+//main function to call each function.
+main = () => {
+  getCurrentTime();
+  getInit();
+  attachBtnClicks();
+  reserveTimeStates();
+};
+main();
